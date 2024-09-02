769a0dcfd40dcefc3d06f41fd8a11c6907efd54d
Takiyo Takahashi <takiyotakahashi@gmail.com>
2024-09-02 13:28:33 +0700

    [C] prevent user from accessing detect methods after done filling form

--- client/app/challenge/page.tsx
+++ client/app/challenge/page.tsx
@@ -2,6 +2,8 @@ import Challenge from "@/app/challenge/challenge";
 import {getCookies, getCookie} from "cookies-next";
 import {cookies} from "next/headers";
 import {ApiManager} from "@/app/managers/api";
+import {redirect} from "next/navigation";
+import ErrorPage from "@/app/components/pages/ErrorPage";
 
 export default async function ChallengePage() {
     const authorization = getCookie("Authorization", {cookies});
@@ -10,6 +12,11 @@ export default async function ChallengePage() {
 
     console.log("parent challenge called")
 
-    const user = await ApiManager.getUser(abort.signal, authorization ?? "");
-    return <Challenge userData={user.data}/>;
+    const {data: user, statusCode} = await ApiManager.getUser(abort.signal, authorization ?? "");
+
+    if (!user.userId) return redirect("/login");
+    if (user.interviewQuestionStatus == "IN_PROGRESS") return redirect("/challenge/interview");
+    if (user.filledSkillInfo) return <ErrorPage message={"Anda sudah tidak bisa memilih opsi lagi disini"}
+                                                errorCode={400}/>
+    return <Challenge userData={user}/>;
 }
