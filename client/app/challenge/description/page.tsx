aa4d31daf603c7f0a368b9ea86dc4a685a3fe9ee
Takiyo Takahashi <takiyotakahashi@gmail.com>
2024-09-02 13:28:02 +0700

    [C] prevent user from accessing form after done it

--- client/app/challenge/description/page.tsx
+++ client/app/challenge/description/page.tsx
@@ -3,12 +3,18 @@ import {getCookies, getCookie} from "cookies-next";
 import {cookies} from "next/headers";
 import {ApiManager} from "@/app/managers/api";
 import ChallengeDescription from "@/app/challenge/description/description";
+import ErrorPage from "@/app/components/pages/ErrorPage";
+import {redirect} from 'next/navigation'
 
 export default async function ChallengeDescriptionPage() {
     const authorization = getCookie("Authorization", {cookies});
 
     const abort = new AbortController();
 
-    const user = await ApiManager.getUser(abort.signal, authorization ?? "");
-    return <ChallengeDescription userData={user.data}/>
+    const {data: user, statusCode} = await ApiManager.getUser(abort.signal, authorization ?? "");
+    if (!user.userId) return redirect("/login");
+    if (user.interviewQuestionStatus == "IN_PROGRESS") return redirect("/challenge/interview");
+    if (user.filledSkillInfo) return <ErrorPage message={"Anda sudah mengisi form deskripsi kemampuan Anda"}
+                                                errorCode={400}/>
+    return <ChallengeDescription userData={user}/>
 }
