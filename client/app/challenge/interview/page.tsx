5774096264236b0a879414bf818c975962cb3ea9
Takiyo Takahashi <takiyotakahashi@gmail.com>
2024-09-05 21:50:13 +0700

    [C] prevent not logged-in user access interview page

--- client/app/challenge/interview/page.tsx
+++ client/app/challenge/interview/page.tsx
@@ -2,13 +2,20 @@ import {getCookies, getCookie} from "cookies-next";
 import {cookies} from "next/headers";
 import {ApiManager} from "@/app/managers/api";
 import ChallengeInterview from "@/app/challenge/interview/Interview";
+import {redirect} from "next/navigation";
+import ErrorPage from "@/app/components/pages/ErrorPage";
 
 export default async function ChallengeInterviewPage() {
     const authorization = getCookie("Authorization", {cookies});
 
     const abort = new AbortController();
 
-    const user = await ApiManager.getUser(abort.signal, authorization ?? "");
+    const {data: user, statusCode} = await ApiManager.getUser(abort.signal, authorization ?? "");
+    if (statusCode != 200) return redirect("/login");
+    if (user.interviewQuestionStatus == 'SUCCESS') return <ErrorPage
+        message={"Anda sudah mengisi interview. Untuk mengakses pembahasan, silahkan buka Arsip Pertanyaan di homepage"}
+        errorCode={401}/>
     const interviewQuestions = await ApiManager.GetInterviewQuestions(abort.signal, authorization ?? "");
-    return <ChallengeInterview userData={user.data} questions={interviewQuestions.data}/>;
+    console.log(interviewQuestions.data.questions)
+    return <ChallengeInterview userData={user} questions={interviewQuestions.data}/>;
 }
