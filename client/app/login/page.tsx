c776c8b1ae11106bc1c8849c05ddb9af337bf556
Takiyo Takahashi <takiyotakahashi@gmail.com>
2024-10-05 23:52:40 +0700

    [C] fix login page being a static page

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -1,44 +1,13 @@
-﻿import {LogoComponent} from "@/app/assets/images/logo";
+﻿'use client'
+
 import {Manrope} from "next/font/google";
-import {Button} from "@nextui-org/react";
-import {FaGithub} from "react-icons/fa";
-import {setCookie} from "cookies-next";
 import {ApiManager} from "@/app/managers/api";
-import CookieManager from "@/app/components/cookie";
-import {MdError} from "react-icons/md";
-import {Metadata} from "next";
+import LoginPage from "@/app/login/LoginPage";
 
 const manrope = Manrope({subsets: ["latin"]});
 
-export const metadata: Metadata = {
-    title: 'Kuduga AI',
-    description: 'platform untuk belajar pemrograman',
-};
 export default async function Login() {
     const {data, statusCode} = await ApiManager.GetOauthCode();
 
-    return <main
-        className={"w-screen h-screen " + manrope.className}>
-        {statusCode == 200 && <CookieManager type={"SET_OAUTH"} value={data.state}/>}
-        <div
-            className={"bg-[#00000052] w-[90vw] max-w-xl relative left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 p-8 rounded-3xl shadow-2xl drop-shadow-2xl"}>
-            <LogoComponent viewBox={"0 0 35 28"} className={"w-28 h-28 mx-auto"}/>
-            <h1 className={"text-3xl font-semibold text-center mt-2"}>Kuduga AI</h1>
-            <h3 className={"text-center mt-2 text-xl"}>Identify your core skills and interests</h3>
-            {statusCode != 200 && <div className={"bg-red-500 w-full rounded-xl p-4 box-border mt-5 flex items-center"}>
-                <MdError size={40}/>
-                <p className={"text-[.9rem] ml-3"}>There was an unexpected error communicating to server. Please try
-                    again in
-                    a few minutes</p>
-            </div>}
-            <div className={"min-h-32 w-full max-w-96 ml-auto mr-auto flex items-center"}>
-                <a className={"w-full"} href={data.loginUrl}>
-                    <Button color="primary" className={"w-full text-[1.1rem] h-12"} disabled={statusCode != 200}
-                            startContent={<FaGithub size={30}/>}>
-                        Login with GitHub
-                    </Button>
-                </a>
-            </div>
-        </div>
-    </main>
+    return <LoginPage data={data} statusCode={statusCode}/>
 }
